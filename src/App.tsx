b76286affbe25eb7348189044fa863430206ea40
Ming-der Wang <mingderwang@gmail.com>
2021-10-10 12:24:51 +0800

    init for deploy example itself on netlify

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { VideoPlayerComponent } from 'videoflowonline-streamer'
+
+const App = () => {
+  return (
+    <>
+      <VideoPlayerComponent />
+    </>
+  )
+}
+
+export default App
